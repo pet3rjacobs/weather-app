ffcdb8b928dfd22b26fadd08d4bf41b9a8925b73
Peter <peterjacobs717@gmail.com>
2022-08-06 11:25:37 +0200

    Added Search Component + API call to fill accordion

---
+++ weather-app/src/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bafffb0e65msh784fe03b9d5aa6ap1bce82jsn1228ff51e05a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,14 @@
+import Search from './Components/Search/Search';
 import './App.css';
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log('Hi');
+  };
+
   return (
-    <div className="App">
-      <p>Test</p>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
